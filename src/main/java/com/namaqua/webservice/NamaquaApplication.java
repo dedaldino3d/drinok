5fe9fb26d6f8334a23a2a151e1eb33e7ced8970c
Dedaldino3D <dedaldinoantonio7@gmail.com>
2021-02-03 13:58:00 +0100

    First commit

---
+++ src/main/java/com/namaqua/webservice/NamaquaApplication.java
@@ -0,0 +1,13 @@
+package com.namaqua.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamaquaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamaquaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/namaqua/webservice/NamaquaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.namaqua.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamaquaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
